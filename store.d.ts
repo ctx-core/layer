ccb1c9eb18ce246b1124914b18c3bf8c0df23b9b
Brian Takita <brian.takita@gmail.com>
2020-10-02 16:43:58 -0700

    type updates

--- store.d.ts
+++ store.d.ts
@@ -10,8 +10,8 @@ export interface type__a1__layer extends Writable<Layer[]> {
 export declare const b__a1__layer: (ctx?: object | undefined, opts?: import("@ctx-core/object")._be_opts_type | undefined) => type__a1__layer;
 export declare const __a1__layer: type__a1__layer;
 export declare const push__a1__layer: (...a1__layer__: Layer[]) => void, unshift__a1__layer: (...a1__layer__: Layer[]) => void, remove__a1__layer: (...a1__layer__: Layer[]) => void;
-export declare const b__top__layer: (ctx?: object | undefined, opts?: import("@ctx-core/object")._be_opts_type | undefined) => any;
-export declare const __top__layer: any;
+export declare const b__top__layer: (ctx?: object | undefined, opts?: import("@ctx-core/object")._be_opts_type | undefined) => Readable<import("@ctx-core/function").maybe<Layer, undefined>>;
+export declare const __top__layer: Readable<import("@ctx-core/function").maybe<Layer, undefined>>;
 export declare type $zIndex__top__layer = number;
 export declare type zIndex__top__layer = Readable<$zIndex__top__layer>;
 export declare const b__zIndex__top__layer: (ctx?: object | undefined, opts?: import("@ctx-core/object")._be_opts_type | undefined) => zIndex__top__layer;
