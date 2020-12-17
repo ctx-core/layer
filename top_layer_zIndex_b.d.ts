6a74e1bb7a4acdbb1b73447e47e2a16604307eb8
Brian Takita <brian.takita@gmail.com>
2020-12-16 23:07:47 -0700

    interface instead of type: top_layer_zIndex_type

--- top_layer_zIndex_b.d.ts
+++ top_layer_zIndex_b.d.ts
@@ -1,4 +1,5 @@
 import { Readable } from '@ctx-core/store';
-export declare const top_layer_zIndex_b: (ctx: object, opts?: import("@ctx-core/object")._be_opts_type | undefined) => zIndex__top__layer;
-export declare type $zIndex__top__layer = number;
-export declare type zIndex__top__layer = Readable<$zIndex__top__layer>;
+export declare const top_layer_zIndex_b: (ctx: object, opts?: import("@ctx-core/object")._be_opts_type | undefined) => top_layer_zIndex_type;
+export declare type $top_layer_zIndex_type = number;
+export interface top_layer_zIndex_type extends Readable<$top_layer_zIndex_type> {
+}
--- top_layer_zIndex_b.ts
+++ top_layer_zIndex_b.ts
@@ -1,10 +1,12 @@
-import { derived, Readable } from '@ctx-core/store'
 import { _b } from '@ctx-core/object'
 import { _andand } from '@ctx-core/function'
+import { derived, Readable } from '@ctx-core/store'
 import { top_layer_b } from './top_layer_b'
-export const top_layer_zIndex_b = _b<zIndex__top__layer>('top_layer_zIndex', ctx=>
+export const top_layer_zIndex_b = _b('top_layer_zIndex', ctx=>
 	derived(
 		top_layer_b(ctx),
-		_andand('zIndex')))
-export type $zIndex__top__layer = number
-export type zIndex__top__layer = Readable<$zIndex__top__layer>
+		_andand('zIndex')
+	) as top_layer_zIndex_type
+)
+export type $top_layer_zIndex_type = number
+export interface top_layer_zIndex_type extends Readable<$top_layer_zIndex_type> {}
