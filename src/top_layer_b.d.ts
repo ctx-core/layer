3ffede9a74f3e1163c77fbe577adaa9297bbedeb
Brian Takita <brian.takita@gmail.com>
2021-02-23 18:17:31 -0500

    update dependencies

--- src/top_layer_b.d.ts
+++ src/top_layer_b.d.ts
@@ -1,1 +1,1 @@
-export declare const top_layer_b: import("@ctx-core/object").be_type<import("@ctx-core/store").Readable<import("@ctx-core/function").maybe<import("./layer_a1_b").Layer, undefined>>, object>;
+export declare const top_layer_b: import("@ctx-core/object").be_type<import("@ctx-core/store").Readable<import("./layer_a1_b").Layer | undefined>, object>;
