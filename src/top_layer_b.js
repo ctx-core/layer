230cac679e44cb2c5732d51e1b5f51c894f9e8ed
Brian Takita <brian.takita@gmail.com>
2021-05-25 01:29:11 -0400

    ## 9.4.0
    ### Minor Changes
    
    - fix tsc build directory issues
    
    ### Patch Changes
    
    - Updated dependencies [undefined]
    - @ctx-core/array@20.5.0
    - @ctx-core/error@8.3.0
    - @ctx-core/function@17.11.0
    - @ctx-core/object@18.2.0
    - @ctx-core/store@24.10.0

--- src/top_layer_b.js
+++
@@ -1,6 +0,0 @@
-import { _b } from '@ctx-core/object';
-import { _last } from '@ctx-core/array';
-import { derived$ } from '@ctx-core/store';
-import { layer_a1_b } from './layer_a1_b';
-const key = 'top_layer';
-export const top_layer_b = _b(key, ctx => derived$(layer_a1_b(ctx), $layer_a1 => _last($layer_a1)));
